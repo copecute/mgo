d7a9b341c2c3153be4a83ba15a535032584d1434
copecute <copecute10x@gmail.com>
2025-03-23 03:54:30 +0700

    build lần 2

--- mGO.service.worker.js
+++ mGO.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1742610603|806668689';
+const CACHE_VERSION = '1742676839|2734611723';
 /** @type {string} */
 const CACHE_PREFIX = 'mGO-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
